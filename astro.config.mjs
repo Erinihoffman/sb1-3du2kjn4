f7e8a0ff1ccee24198c22f63689ebe90d6c95d18
Erinihoffman <kristiankostov53@gmail.com>
2025-01-16 16:34:42 +0200

    Fix config file changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  publicDir: './public', // Ensure the public directory is correctly set
+  site: 'https://erinihoffmann.com', // Replace with your site domain
 });
