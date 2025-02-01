0da8a8293d6f0dff5ab8e655611817b51493277c
Erinihoffman <kristiankostov53@gmail.com>
2025-02-01 17:49:30 +0200

    everything

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(), // TailwindCSS integration
+    react(),    // React integration
+  ],
   publicDir: './public', // Ensure the public directory is correctly set
   site: 'https://erinihoffmann.com', // Replace with your site domain
 });
